f08fe6293a82aa19cac083e5761427da6ae00522
HyunJeongC <airbcss@naver.com>
2022-10-09 15:18:08 +0900

    Initial commit

---
+++ src/main/java/com/spring/oop/inherit/Warrior.java
@@ -0,0 +1,28 @@
+package com.spring.oop.inherit;
+
+public class Warrior extends Player {
+
+    private int rage;
+
+//    public Warrior(){
+//        super();
+//        System.out.println("Warrior 기본 생성자 호출!");
+//
+//    }
+
+    public Warrior(String nickName) {
+        super(nickName);
+        this.rage = 100;
+        System.out.println("Warrior 생성자 호출!");
+
+    }
+
+    @Override
+    public void info() {
+        super.info();
+        System.out.println("# 분노: " + rage);
+        System.out.println("========================");
+    }
+
+
+}
